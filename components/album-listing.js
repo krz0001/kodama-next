cb0b077f78a0354276ca3af393ae746fd67e7e26
krz <maxlav78@gmail.com>
2022-04-07 23:34:50 +0200

    Update album-listing.js

--- components/album-listing.js
+++ components/album-listing.js
@@ -9,11 +9,13 @@ export default function AlbumListing({ album }) {
     const { t } = useTranslation('common')
 
     return (
-        <div className="overflow-visible group relative max-w-xl mx-auto border-solid border border-violet-500/50 hover:border-violet-500 rounded-xl flex items-center gap-6 my-5 transition">
-            <div className="absolute -left-6 w-52 h-52 rounded-md shadow-lg group-hover:scale-105 transition overflow-clip">
+        <div className="group relative max-w-2xl mx-auto content-center border-solid border border-violet-500/50 hover:border-violet-500 rounded-xl grid grid-cols-3 my-5 transition">
+
+            <div className="relative h-5/6 mx-auto my-5 aspect-square rounded-md shadow-lg transition overflow-clip">
                 <Image src={album.cover} alt={album.name} layout="fill" sizes="13rem" priority placeholder='blur' blurDataURL={album.coverColor}/>
             </div>
-            <div className="flex flex-col py-5 pl-52">
+
+            <div className="col-span-2 flex flex-col py-5 px-10">
                 <div>
                     <span className="font-mono text-sm">{album.id}</span>
                     <h2 className="text-2xl font-semibold">{album.name}</h2>
