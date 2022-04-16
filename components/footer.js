fb4a7d0bfa7be0596363a9ab770889e385a9b38e
krz <maxlav78@gmail.com>
2022-04-16 15:19:46 +0200

    Updated footer

--- components/footer.js
+++ components/footer.js
@@ -1,22 +1,19 @@
 
-import {FaTwitter, FaBandcamp, FaFacebook, FaDiscord} from 'react-icons/fa';
+import {FaTwitter, FaBandcamp, FaDiscord} from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className="border-t border-slate-500/20 mt-10">
       <div className="container max-w-4xl mx-auto flex py-8 flex-col text-center">
           <div className="w-full mx-auto flex justify-center flex-wrap items-center gap-3">
-              <a href="" className='footer_svg_container outline-none'>
+              <a href="http://twitter.com/kodamasoft" className='footer_svg_container outline-none'>
                 <FaTwitter
                  className='text-slate-500 no-underline hover:text-slate-50 hover:scale-105 w-12 h-12 transition'/>                  
               </a>
-              <a href="" className="footer_svg_container outline-none">
+              <a href="https://kodamasounds.bandcamp.com/" className="footer_svg_container outline-none">
                 <FaBandcamp className='text-slate-500 no-underline hover:text-slate-50 hover:scale-105 w-12 h-12 transition'/> 
               </a>
-              <a href="" className="footer_svg_container outline-none">
-                <FaFacebook className='text-slate-500 no-underline hover:text-slate-50 hover:scale-105 w-12 h-12 transition'/> 
-              </a>
-              <a href="" className="footer_svg_container outline-none">
+              <a href="https://discordapp.com/invite/9ZmHv3r" className="footer_svg_container outline-none">
                 <FaDiscord className='text-slate-500 no-underline hover:text-slate-50 hover:scale-105 w-12 h-12 transition'/> 
               </a>
           </div>
