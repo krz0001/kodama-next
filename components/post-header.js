7df5a2630c0285b01b26a42e09e8087ab11a33d5
krz <maxlav78@gmail.com>
2022-04-20 21:19:36 +0200

    Started work on project listings

--- components/post-header.js
+++ components/post-header.js
@@ -8,13 +8,14 @@ export default function PostHeader({ title, date, author, twin }) {
   return (
     <div className='max-w-prose	mx-auto border-b pb-4 mb-4 border-slate-500/20 sm:text-center' >
       <h1 className='text-4xl	font-extrabold mb-3'>{title}</h1>
-      <p>{t('words.written_by')}<span className="text-violet-500"> {author}</span></p>
+      {/* <p>{t('words.written_by')}<span className="text-violet-500"> {author}</span></p> */}
+      <p>Written by <span className="text-violet-500"> {author}</span></p>
       <div className="text-slate-400">
         <DateFormatter dateString={date} />
       </div>
       { twin ? 
       ( <Link href={"/posts/" + twin.slug}>
-          <a>Check {twin.locale.toUpperCase()} article ({twin.title})</a>
+          <a>Check {twin.locale.toUpperCase()} article</a>
       </Link> ) : null }
     </div>
   )
