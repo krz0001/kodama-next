0e06df69c2599ea899b4019ca3fc34f4bb0d8246
krz <maxlav78@gmail.com>
2022-08-02 00:41:58 +0200

    Start of wrappable sections

---
+++ components/release/album/release-track.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import trackStyles from './release-track.module.scss'
+
+
+export default function ReleaseTrack({track, key}) {
+
+  // State to show/hide accordion
+  const [show, setShow] = useState(false);
+  const handleOpen = () => {
+    setShow(!show); // Toggle accordion
+  };
+
+  return (
+      <div className="accordion" key={key}>
+        <div className="container max-w-screen-lg mx-auto mb-4" onClick={handleOpen}>
+            <div className={trackStyles.track_number}>
+                {track[0].padStart(2, '0')}
+            </div>
+            <div className='text-xl font-bold text-left select-none cursor-pointer'>
+                {track[1].title}
+            </div>
+            <div>
+                {show ? "V" : "^" }
+            </div>
+        </div>
+        {show && (
+          <div className="accordian-body bg-[#18191b] text-white py-4">
+            <div className="container max-w-screen-lg mx-auto mt-4">
+            {track[1].description.map((descParagraph, index)  => {
+                return (
+                    <p key={index} className="mb-2 text-sm">
+                        {descParagraph}
+                    </p>
+                )
+            })}
+            </div>
+          </div>
+        )}
+      </div>
+    
+  );
+}
