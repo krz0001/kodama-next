7c793320cd0885e4bec8acac0f7dd9e53d9eb632
krz <maxlav78@gmail.com>
2022-08-02 00:41:58 +0200

    Start of wrappable sections

--- components/release/album/release-tracklist.js
+++ components/release/album/release-tracklist.js
@@ -1,33 +1,15 @@
-import tracklistStyles from './release-tracklist.module.scss'
+import ReleaseTrack from './release-track';
 
 export default function ReleaseTracklist({tracklist}) {
 	return (
-		<section className='container mx-auto my-16 max-w-screen-lg'>
+		<section className='mx-auto my-16'>
             <h2 className="text-2xl text-center uppercase mb-8 font-black"> tracklist </h2>
+
             {Object.entries(tracklist).map(trackJSON => {
                 console.log(trackJSON);
 
                 return (
-                    <div key={trackJSON[0]} className={tracklistStyles.track_container}>
-                        <div className={tracklistStyles.track_number}>
-                            {trackJSON[0].padStart(2, '0')}
-                        </div>
-                        <div className={tracklistStyles.track_content} data-accordion="open">
-                            <button type="button" className='text-xl font-bold text-left' data-accordion-target={"accordion-arrow-icon-body-"+ trackJSON[0]} aria-expanded="false">
-                                {trackJSON[1].title}
-                            </button>
-
-                            <div id={"accordion-arrow-icon-body-"+ trackJSON[0]} className="hidden">
-                                {trackJSON[1].description.map((descParagraph, index)  => {
-                                    return (
-                                        <p key={index} className="text-[#999] mb-4 text-sm	">
-                                            {descParagraph}
-                                        </p>
-                                    )
-                                })}  
-                            </div>
-                        </div>
-                    </div>
+                    <ReleaseTrack track={trackJSON} key={trackJSON[0]} />
                 )
             })}
         </section>
