05ac91086dcaeccb6887b8613d4f53a4edb91353
krz <maxlav78@gmail.com>
2022-08-21 00:34:37 +0200

    Fully updated JSON format and first design of project listing v2

---
+++ components/mdx-layout.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+
+import Layout from './layout'
+import Header from './header'
+import ProseContainer from './prose-container'
+import { WEBSITE_NAME } from '../lib/constants'
+
+
+
+export default function ProjectLayout({ title ,children }) {
+
+
+    return (
+        <Layout>
+            <Head>
+                <title>{ title ? title + "-" + WEBSITE_NAME : WEBSITE_NAME }</title>
+            </Head>
+            <Header />
+            <div className="container pt-10 px-6 mx-auto">
+                <ProseContainer>
+                    { children }
+                </ProseContainer>
+            </div>
+        </Layout>
+    )
+}
+
+
--- components/project-listing.js
+++ components/project-listing.js
@@ -1,19 +1,33 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function ProjectListing({ project }) {
-    console.log(project);
+	console.log(project)
 
-    return (
-        <Link href={project.url}>
-            <a className="relative group w-full rounded-md overflow-hidden h-28">    
-                <h3 className="text-2xl  mt-5 subpixel-antialiased">{project.name}</h3>
-
-                <div className="w-auto bg-gray-200 h-2 rounded-full overflow-hidden mt-3">
-                    <div className="bg-purple-600 h-2 rounded-full" style={{"width": Math.floor(Math.random() * 100) + "%"}}></div>    
-                </div>
-
-            </a>
-        </Link>
-      )
+	return (
+		<Link href={project.url}>
+			<a className="block w-full no-underline not-prose mb-8">
+				<h3 className="text-2xl subpixel-antialiased mt-0">
+					{project.title}
+				</h3>
+				<p>
+					Theme:
+					{' ' +
+						project.themes.map(function (theme, index) {
+							return (index > 0 ? ' ' : '') + theme
+						})}
+				</p>
+				<p>
+					Status: {project.status} | {project.percentage}% |{' '}
+					{project.duration}
+				</p>
+				<div className="w-full bg-white h-2 rounded-full overflow-hidden ">
+					<div
+						className="bg-purple-600 h-2 rounded-full"
+						style={{ width: project.percentage + '%' }}
+					></div>
+				</div>
+				{project.deadline && <p>Deadline: {project.deadline}</p>}
+			</a>
+		</Link>
+	)
 }
