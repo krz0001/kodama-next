81e6d9d6e19f7a4f5de4c655a72e844cda0fcff7
krz <maxlav78@gmail.com>
2022-11-11 15:25:18 +0100

    Reworked dropdown sections with HeadlessUI

--- components/release/album/release-track.js
+++ components/release/album/release-track.js
@@ -1,43 +1,46 @@
-import React, { useState } from "react";
+import { Disclosure, Transition } from '@headlessui/react'
+
 import trackStyles from './release-track.module.scss'
 
 
 export default function ReleaseTrack({track, key}) {
 
-  // State to show/hide accordion
-  const [show, setShow] = useState(false);
-  const handleOpen = () => {
-    setShow(!show); // Toggle accordion
-  };
 
   return (
-      <div className="accordion" key={key}>
-        <div className="container max-w-screen-lg mx-auto mb-4  flex	md:px-8" onClick={handleOpen}>
-            <div className={trackStyles.track_number}>
-                {track[0].padStart(2, '0')}
-            </div>
-            <div className='text-xl font-bold text-left select-none cursor-pointer '>
-                {track[1].title} 
-                <span className="ml-3">
-                  {show ?  "▲" : "▼"}
-                </span>
-            </div>
-            
-        </div>
-        {show && (
-          <div className="accordian-body bg-[#18191b] text-white shadow-inner-xl py-4 mb-4">
-            <div className="container max-w-screen-lg mx-auto mt-4">
-            {track[1].description.map((descParagraph, index)  => {
-                return (
-                    <p key={index} className="mb-2 text-sm">
-                        {descParagraph}
-                    </p>
-                )
-            })}
-            </div>
-          </div>
-        )}
-      </div>
+
+    
+      <Disclosure className="accordion mb-4" key={key} as="div">
+              <Disclosure.Button className="container max-w-screen-lg mx-auto mb-4 flex	px-2 md:px-8">
+                <div className={trackStyles.track_number}>
+                  {track[0].padStart(2, '0')}
+                </div>
+                <div className="text-xl font-bold text-left select-none cursor-pointer ">
+                  {track[1].title} 
+                </div>
+              </Disclosure.Button>
+              
+              <Transition
+                enter="transition duration-100 ease-out origin-top"
+                enterFrom="transform scale-y-0 opacity-0 height-0"
+                enterTo="transform scale-y-100 opacity-100 height-auto"
+                leave="transition duration-100 ease-out origin-top"
+                leaveFrom="transform scale-y-100 opacity-100 height-auto"
+                leaveTo="transform scale-y-0 opacity-0 height-0"
+              >
+                <Disclosure.Panel className="accordian-body bg-[#18191b] text-white shadow-inner-xl py-4 px-4 mb-4 ">
+                  <div className="container max-w-screen-lg mx-auto mt-4">
+                    {track[1].description.map((descParagraph, index)  => {
+                        return (
+                            <p key={index} className="mb-2 text-sm">
+                                {descParagraph}
+                            </p>
+                        )
+                    })}
+                  </div>
+                </Disclosure.Panel>
+              </Transition>
+      </Disclosure>
+      
     
   );
 }
