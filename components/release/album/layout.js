d6543bbef6c06e0084f4c775b0511fedcd4f7089
krz <maxlav78@gmail.com>
2022-12-26 00:49:03 +0100

    Added description and favicon

--- components/release/album/layout.js
+++ components/release/album/layout.js
@@ -23,6 +23,24 @@ export default function ReleaseLayout({ release }) {
         <>
             <GlobalStyles />
             <Head>
+                <link rel="shortcut icon" href="/favicons/favicon.ico" />
+                <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
                 <title>{release.title + " - KodamaSounds"}</title>
                 <meta property="og:title" content={release.title + " - KodamaSounds"} />
                 <meta name="theme-color" content={'#' + release.color} />
