6d7efa108be1b1c848d73c760ccda96541a92c98
krz <maxlav78@gmail.com>
2022-12-25 22:35:09 +0100

    Finish updated credits section

--- components/release/album/release-credits.js
+++ components/release/album/release-credits.js
@@ -1,34 +1,109 @@
 import Link from 'next/link'
+import collaboratorsJson from '/_collaborators.json'
+import {
+	FaBandcamp,
+	FaGlobe,
+	FaYoutube,
+	FaSoundcloud,
+	FaTwitter,
+} from 'react-icons/fa'
+import BoothSVG from '../../../public/assets/icons/booth.svg'
+import PixivSVG from '../../../public/assets/icons/pixiv.svg'
+import FanboxSVG from '../../../public/assets/icons/fanbox.svg'
 
 import tracklistStyles from './release-credits.module.scss'
 
 export default function ReleaseTracklist({ credits }) {
+	function LinkIcon(props) {
+		console.log(props)
+		const link = props['linkObj']
+
+		if (link.toString().includes('bandcamp')) {
+			return <FaBandcamp className={props.className} />
+		} else if (
+			link.toString().includes('youtube') ||
+			link.toString().includes('youtu.be')
+		) {
+			return <FaYoutube className={props.className} />
+		} else if (link.toString().includes('twitter')) {
+			return <FaTwitter className={props.className} />
+		} else if (link.toString().includes('soundcloud')) {
+			return <FaSoundcloud className={props.className} />
+		} else if (link.toString().includes('vgmdb')) {
+			return <span className={props.className}>VGMDB</span>
+		} else if (link.toString().includes('booth.pm')) {
+			return <BoothSVG className={props.className} />
+		} else if (link.toString().includes('pixiv')) {
+			return <PixivSVG className={props.className} />
+		} else if (link.toString().includes('fanbox.cc')) {
+			return <FanboxSVG className={props.className} />
+		} else {
+			return <FaGlobe className={props.className} />
+		}
+	}
+
 	return (
-		<section className='container mx-auto my-16'>
-            <h2 className="text-2xl text-center uppercase mb-8 font-black"> credit </h2>
-            <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 '>
-                {Object.entries(credits).map(creditJSON => {
-                    return (
-                        <li key={creditJSON[0]} className="text-center">
-                            <span className='block text-sm text-[#999]'>
-                                {creditJSON[1].role}
-                            </span>
+		<section className="container mx-auto my-16">
+			<h2 className="text-2xl text-center uppercase mb-8 font-black">
+				{' '}
+				credit{' '}
+			</h2>
 
-                            <span className='block text-xl font-bold my-2'>
-                                {creditJSON[1].name}
-                            </span>
+			{/* make it a column on mobile and then a table on bigger displays*/}
+			<table class="table-auto w-full">
+				<thead className='hidden md:table-header-group'>
+					<tr>
+						<th className="px-4 py-2">Name</th>
+						<th className="px-4 py-2">Role</th>
+						<th className="px-4 py-2">Link</th>
+					</tr>
+				</thead>
+				<tbody className="grid grid-cols-1 sm:grid-cols-2 p-4 gap-4 md:table-row-group md:p-0 md:border-t border-[#666]">
+					{Object.entries(credits).map((creditJSON) => {
+						let collaboratorInfo =
+							collaboratorsJson[creditJSON[1].id]
+						console.log(collaboratorInfo)
 
-                            <span className={tracklistStyles.button}>
-                                <Link href={creditJSON[1].website} passHref target="_blank">
-                                    website
-                                </Link>                            
-                            </span>
-                        
-                        </li>
-                    )
-                })}
-            </ul>
-
-        </section>
+						const tdClass = 'md:border-b border-[#666] md:p-4 md:pl-8'
+						return (
+							<tr key={creditJSON[0]} className="flex flex-col gap-4 w-auto pb-4 mb-4 border-b border-[#666] sm:border-0 md:m-0 md:table-row md:w-full">
+								<td className={tdClass}>
+									{collaboratorInfo.name}
+								</td>
+								<td className={`text-white/50 ${tdClass}`}>
+									{creditJSON[1].role}
+								</td>
+								<td className={tdClass}>
+									<div className="flex flex-wrap gap-4">
+										{collaboratorInfo.links &&
+											Object.entries(
+												collaboratorInfo.links
+											).sort((a, b) => {
+												if (a[0] < b[0]) { return -1 }
+												if (a[0] > b[0]) { return 1 }
+												return 0
+											}).map((link) => {
+												return (
+													<Link
+														href={link[1]}
+														passHref
+														target="_blank"
+														key={link[0]}
+													>
+														<LinkIcon
+															className="min-w-6 w-auto fw-bold h-6 hover:text-[color:var(--release-color)] focus:text-[color:var(--release-color)] transition-all duration-300 ease-in-out"
+															linkObj={link[1]}
+														/>
+													</Link>
+												)
+											})}
+									</div>
+								</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</section>
 	)
 }
--- components/release/album/release-nav.js
+++ components/release/album/release-nav.js
@@ -17,7 +17,7 @@ export default function ReleaseNav({...props}) {
 							alt="KodamaSoft"
 						/>
 					</Link>
-					<div className="flex items-center bg-slate-800/50 rounded-lg p-2">
+					<div className="flex items-center bg-neutral-700/50 rounded-lg p-2">
 						<LocaleSwitcher />
 					</div>
 				</div>
