99b4d37887b86e29ac53b24d4dbe7cf473b5b361
krz <maxlav78@gmail.com>
2023-06-08 23:40:56 +0200

    New blogpost

--- components/project-information.js
+++ components/project-information.js
@@ -4,6 +4,7 @@ import useTranslation from 'next-translate/useTranslation'
 export default function ProjectInfo({ slug, children }) {
     const { t, lang } = useTranslation();
     let project = {};
+    let category = {};
 
     // iterate trough the array of sections in the projectsJSON file
     for (let i = 0; i < projectsJSON.length; i++) {
@@ -11,6 +12,7 @@ export default function ProjectInfo({ slug, children }) {
         for (let j = 0; j < projectsJSON[i].projects.length; j++) {
             // if the project has the same slug as the one we are looking for, return the project
             if (projectsJSON[i].projects[j].slug === slug) {
+                category = projectsJSON[i];
                 project = projectsJSON[i].projects[j];
                 break;
             }
@@ -20,12 +22,12 @@ export default function ProjectInfo({ slug, children }) {
 	return (
 		<p>
             <strong>Album Title:</strong> {t('projects:' + category.cat_slug + '.' + project.slug + '.title')}<br/>
-            <strong>Status:</strong> { project.status ? project.status : " ??? "}<br/>
+            <strong>Status:</strong> { t('projects:common.statuses.' + project.status) }<br/>
             <strong>Approximate Completion:</strong> { project.percentage ? project.percentage : " ??? "}%<br/>
             <strong>Current Duration:</strong> { project.duration ? project.duration : " ??? "}<br/>
             <strong>Current Number of Finished Tracks:</strong> { project.finished_tracks ? project.finished_tracks : " ??? "}<br/>
             <strong>Mastering Engineer:</strong> { project.mastering.engineer ? project.mastering.engineer : " ??? "}<br/>
-            <strong>Mastering Status:</strong> { project.mastering.status ? project.mastering.status : " ??? "}<br/>
+            <strong>Mastering Status:</strong> { t('projects:common.statuses.' + project.mastering.status) }<br/>
             <strong>Artwork Commission:</strong> { project.artwork_comission ? project.artwork_comission : " ??? "}<br/>
 		</p>
 	)
