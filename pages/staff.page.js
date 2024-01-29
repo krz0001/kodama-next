a36f0cf9b98d5e7db1df2522918531a808f2786e
krz <maxlav78@gmail.com>
2024-01-29 23:12:27 +0100

    Refactor staff page component to handle legacy staff members

--- pages/staff.page.js
+++ pages/staff.page.js
@@ -31,23 +31,30 @@ export default function Staff({ }) {
                         <div className='max-w-4xl mx-auto font-medium'>
 
                             {staffJson.map((staff, index) => (
-                                <div key={index} className="flex flex-col md:grid md:grid-cols-[150px,1fr] gap-4 mb-10">
-                                    <div className="">
-                                        <Image src={"/assets/staff/pictures/kodama_" + staff._collabId + ".png"} alt={staff.name} className="w-full rounded-lg" width={150} height={150} />
+                                (typeof staff._legacy === 'undefined') ?
+                                    <div key={index} className="flex flex-col md:grid md:grid-cols-[150px,1fr] gap-4 mb-10">
+                                        <div className="">
+                                            <Image src={"/assets/staff/pictures/kodama_" + staff._collabId + ".png"} alt={staff.name} className="w-full rounded-lg" width={150} height={150} />
+                                        </div>
+                                        <div className="">
+                                            <h3 className="text-xl font-bold mb-2">{staff.name}</h3>
+                                            <p className="mb-2">{staff.role}</p>
+                                            <ul className="flex flex-col gap-1 mb-2">
+                                                {staff.realName && <li>Real name: {staff.realName}</li>}
+                                                {staff.pronouns && <li>Pronouns: {staff.pronouns}</li>}
+                                                {staff.dateOfBirth && <li>Date of birth: {staff.dateOfBirth}</li>}
+                                                {staff.placeOfBirth && <li>Place of birth: {staff.placeOfBirth}</li>}
+                                            </ul>
+
+                                        </div>
+                                        <p className="col-span-2 mt-2 w-full">{staff.description}</p>
                                     </div>
-                                    <div className="">
+                                    :
+                                    <div key={index} className="mb-10">
                                         <h3 className="text-xl font-bold mb-2">{staff.name}</h3>
                                         <p className="mb-2">{staff.role}</p>
-                                        <ul className="flex flex-col gap-1 mb-2">
-                                            {staff.realName && <li>Real name: {staff.realName}</li>}
-                                            {staff.pronouns && <li>Pronouns: {staff.pronouns}</li>}
-                                            {staff.dateOfBirth && <li>Date of birth: {staff.dateOfBirth}</li>}
-                                            {staff.placeOfBirth && <li>Place of birth: {staff.placeOfBirth}</li>}
-                                        </ul>
-
                                     </div>
-                                    <p className="col-span-2 mt-2 w-full">{staff.description}</p>
-                                </div>
+
                             ))}
                         </div>
 
