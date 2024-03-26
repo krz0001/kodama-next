fed5215d4fc5f1ca0e249206b4d28c712f9f24db
krz <maxlav78@gmail.com>
2024-03-26 21:25:16 +0100

    Add Deezer SVG icon and update release dates in albums.json

--- components/album-listing.js
+++ components/album-listing.js
@@ -1,8 +1,13 @@
-import {FaBandcamp, FaGlobe} from 'react-icons/fa';
+import { FaBandcamp, FaGlobe, FaSpotify } from 'react-icons/fa';
+import { SiApplemusic, SiYoutubemusic } from "react-icons/si";
+
 import BoothSVG from '../public/assets/icons/booth.svg'
+import DeezerSVG from '../public/assets/icons/deezer.svg'
+
 import albumsJSON from '/public/assets/discography/albums.json'
 import Image from "next/image";
 import useTranslation from 'next-translate/useTranslation'
+import DateFormatter from './date-formatter'
 
 export default function AlbumListing({ slug }) {
     let album = {};
@@ -16,27 +21,29 @@ export default function AlbumListing({ slug }) {
             break;
         }
     }
-       
+
     return (
         <div className="not-prose group relative w-full md:max-w-2xl mx-auto content-center border-solid border border-violet-500/50 hover:border-violet-500 rounded-xl flex flex-col md:flex-row my-5 transition">
 
-            <div className="relative h-52 w-52 my-5 mx-auto md:m-5 md:mr-0 aspect-square rounded-md shadow-lg transition overflow-clip flex-none" style={{ backgroundColor : album.coverColor }}>
+            <div className="relative h-52 w-52 my-5 mx-auto md:m-5 md:mr-0 aspect-square rounded-md shadow-lg transition overflow-clip flex-none" style={{ backgroundColor: album.coverColor }}>
                 <Image src={album.cover} alt={album.name} sizes="13rem" quality={100} priority={true} width={208} height={208} />
             </div>
 
             <div className="flex flex-col flex-initial py-5 px-10">
                 <div>
-                    <span className="font-mono text-sm">{album.id}</span>
+                    <span className="font-mono text-sm">
+                        {album.id} — <DateFormatter dateString={album.releaseDate} />
+                    </span>
                     <h2 className="text-2xl font-semibold">{album.name}</h2>
                 </div>
                 <div className="flex flex-wrap mt-4 gap-4">
                     <a href={album.bandcamp} className="kodama_btn kodama_btn--primary">
-                        <FaBandcamp className='h-4 w-4 mr-2'/>
+                        <FaBandcamp className='h-4 w-4 mr-2' />
                         Bandcamp
                     </a>
 
                     <a href={album.booth} className="kodama_btn kodama_btn--primary">
-                        <BoothSVG className='h-4 w-4 mr-2'/>
+                        <BoothSVG className='h-4 w-4 mr-2' />
                         BOOTH
                     </a>
 
@@ -45,19 +52,48 @@ export default function AlbumListing({ slug }) {
                         Website
                     </a>
 
-                    { album.vgmdb ?
-                    <a href={album.vgmdb} className="kodama_btn kodama_btn--primary">
-                        VGMDB
-                    </a>
-                    : null }
+                    {album.vgmdb ?
+                        <a href={album.vgmdb} className="kodama_btn kodama_btn--primary">
+                            VGMDB
+                        </a>
+                        : null}
 
-                    { album.thbwiki ? 
-                    <a href={album.thbwiki} className="kodama_btn kodama_btn--primary">
-                        THB WIKI
-                    </a> 
-                    : null }
+                    {album.thbwiki ?
+                        <a href={album.thbwiki} className="kodama_btn kodama_btn--primary">
+                            THB WIKI
+                        </a>
+                        : null}
+
+                    {album.appleMusic ?
+                        <a href={album.appleMusic} className="kodama_btn kodama_btn--secondary">
+                            <SiApplemusic className='h-4 w-4 mr-2' />
+                            Apple Music
+                        </a>
+                        : null}
+
+                    {album.spotify ?
+                    <a href={album.spotify} className="kodama_btn kodama_btn--secondary">
+                        <FaSpotify className='h-4 w-4 mr-2' />
+                        Spotify
+                    </a>
+                    : null}
+
+                    {album.deezer ?
+                    <a href={album.deezer} className="kodama_btn kodama_btn--secondary">
+                        <DeezerSVG className='h-4 w-4 mr-2' />
+                        Deezer
+                    </a>
+                    : null}
+
+                    {album.youtubeMusic ?
+                    <a href={album.youtubeMusic} className="kodama_btn kodama_btn--secondary">
+                        <SiYoutubemusic className='h-4 w-4 mr-2' />
+                        YouTube Music
+                    </a>
+                    : null}
+
                 </div>
             </div>
         </div>
-      )
+    )
 }
