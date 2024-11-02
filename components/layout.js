311eff9730115a7d3a7728ea99c324edf5640487
krz <maxlav78@gmail.com>
2024-11-02 14:12:01 +0100

    Use a proper typeface to support italics across all glyphs

--- components/layout.js
+++ components/layout.js
@@ -3,12 +3,20 @@ import Meta from '../components/meta'
 
 import layoutStyles from './layout.module.css'
 
+import { Noto_Sans_JP } from "next/font/google"
+
+const noto = Noto_Sans_JP({
+    subsets: ['latin'],
+    weight: ['400', '900'],
+    variable: '--font-noto'
+});
+
 
 export default function Layout({ children }) {
   return (
     <div className={layoutStyles.main_container}>
       <Meta />
-      <div className={layoutStyles.main + " min-h-screen"}>
+      <div className={layoutStyles.main + " " + noto.className + " min-h-screen"}>
         <main>{children}</main>
         <Footer />
       </div>
